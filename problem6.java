dbd7702df5b491b942c39f963d0f1e6bc8e133cd
Harsh Prasad <harshprasad2408@gmail.com>
2025-01-13 19:46:17 +0530

    Add files via upload

---
+++ problem6.java
@@ -0,0 +1,15 @@
+
+import java.util.Scanner;
+
+public class problem6 {
+    public static void main(String[] args) {
+        Scanner input =new Scanner(System.in);
+        System.out.print("Enter the 1st No.");
+        int x = input.nextInt();
+        System.out.print("Enter the 2nd No.");
+        int y = input.nextInt();
+        int sum = x+y;
+        System.out.println("Sum of no are :"+sum);
+    }
+}
+
