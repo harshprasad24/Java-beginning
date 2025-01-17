08303c48cc1a74878e6f58e4e22aa607525780e1
Harsh Prasad <harshprasad2408@gmail.com>
2025-01-17 11:32:08 +0530

    Add files via upload

---
+++ problem25.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class problem25 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the 1st No.");
+        int x = sc.nextInt();
+        System.out.print("Enter the 1st No.");
+        int y = sc.nextInt();
+        int result = x << 1;
+        System.out.println("RESULT:" + result);
+    }
+}
